f20f5a945d401a91c4cdbea9510403f31fce21ce
João Guilherme Ponciano <115370264+joaoponcianoo@users.noreply.github.com>
2025-04-12 10:32:55 -0300

    feat: add Header component with Google auth

---
+++ app/components/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./SideMenu";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 justify-between items-center flex flex-row">
+        <Image src="/logo.png" alt="Barber Logo" height={18} width={120} />
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
---
+++ app/components/SideMenu.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import {
+  Calendar1Icon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  UserIcon,
+} from "lucide-react";
+import { SheetHeader, SheetTitle } from "./ui/sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const SideMenu = () => {
+  const { data } = useSession();
+
+  const handleLogoutClick = () => signOut();
+  const handleLoginClick = () => signIn("google");
+
+  return (
+    <>
+      <SheetHeader className="border-b border-solid border-[var(--secondary)] p-5">
+        <SheetTitle>Menu</SheetTitle>
+      </SheetHeader>
+
+      {data?.user ? (
+        <div className="flex justify-between items-center px-5 py-6">
+          <div className="flex items-center gap-3">
+            <Avatar>
+              <AvatarImage
+                src={data.user?.image || ""}
+                alt="User profile picture"
+              />
+            </Avatar>
+            <h2 className="font-bold">{data.user?.name}</h2>
+          </div>
+          <Button onClick={handleLogoutClick} size="icon">
+            <LogOutIcon />
+          </Button>
+        </div>
+      ) : (
+        <div className="flex flex-col px-5 py-4 gap-3">
+          <div className="flex items-center gap-2">
+            <UserIcon size={32} />
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+          </div>
+          <Button
+            onClick={handleLoginClick}
+            className="bg-[var(--primary)] w-full justify-start"
+          >
+            <LogInIcon className="mr-2" />
+            Fazer Login
+          </Button>
+        </div>
+      )}
+
+      <div className="flex flex-col gap-3 px-5">
+        <Button variant="outline" className="justify-start" asChild>
+          <Link href="/">
+            <HomeIcon size={18} className="mr-2" />
+            Início
+          </Link>
+        </Button>
+      </div>
+
+      {data?.user && (
+        <div className="flex flex-col gap-3 px-5">
+          <Button variant="outline" className="justify-start" asChild>
+            <Link href="/bookings">
+              <Calendar1Icon size={18} className="mr-2" />
+              Agendamentos
+            </Link>
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SideMenu;
